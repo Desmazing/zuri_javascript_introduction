615deb763ed3f0de9c0874dd5c1d84eecb64d38a
Desmond O <100370067+Desmazing@users.noreply.github.com>
2022-06-21 22:59:59 +0300

    printing out information with console.log
    This is a JavaScript program that prints out name, height and country as defined in the given variables.

---
+++ js_intro.js
@@ -0,0 +1,9 @@
+let firstname = 'Desmond';
+let lastname = 'Odera';
+let approxheight = 175;
+let height_units = 'cm';
+let country = 'Kenya';
+
+console.log('My name is '+ firstname + ' ' + lastname + '.');
+console.log('I am approximately ' + approxheight + ' ' + height_units + ' tall.');
+console.log('I am from ' + country + '.');
